5e126949463b39a4ef9e5e532620171800e4a5b6
digital-sirat <dsirat.seo@gmail.com>
2026-08-05 10:29:33 +0000

    feat(navigation): add product foundation route contracts

---
+++ src/navigation/index.ts
@@ -0,0 +1,6 @@
+export * from './featureFlags';
+export * from './legacyCompatibility';
+export * from './navigationSchema';
+export * from './productSections';
+export * from './routeContract';
+export type { ProductRouteMetadata } from './routeMetadata';
